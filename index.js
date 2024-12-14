32364830cc29b0dd8c1a6b559a7dd9fbaa2e3fa0
Ezra <62970784+Etoad8473@users.noreply.github.com>
2024-12-14 13:13:18 +0200

    get all products api endpoint, starting get by id

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/product.model.js')
 const app = express()
 app.use(express.json())
 
@@ -21,10 +22,31 @@ app.get('/', (req,res)=>{
     res.send("Hello from Node API");
 })
 
-app.post('/api/products',(req,res)=>{
+app.post('/api/products', async (req,res) => {
     try {
-        
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
     } catch (error) {
         console.log(error)
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/api/products', async (req,res)=>{
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+})
+
+//TODO
+app.get('/api/products/:id', async (req,res)=>{
+    try {
+        const products = await Product.find({id})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message:error.message})
     }
 })
